03b13ac32fde2f2622d5810771fb035a2f0e5254
nhasan97 <nh6069325@gmail.com>
2024-10-10 08:47:21 +0600

    created USER_ROLE, USER_STATUS, UserSearchableFields constants in user.constant.ts, created userRegister, getAllUsers, getAllAdmins, getSingleUser and updateUserStatus controller functions in user.controller.ts, created TUser type and IUserModel interface in user.inteface.ts, created userSchema, isUserExistsByEmail, isPasswordMatched, isJWTIssuedBeforePasswordChanged static functions and User model in user.model.ts, created a post route, a patch route and 3 get routes in user.route.ts, created createUser, getAllUsersFromDB, getAllAdminsFromDB, getSingleUserFromDB and updateUserStatus service functions in user.service.ts, created createUserValidationSchema and updateUserValidationSchema in user.validation.ts

---
+++ B3-A6-Recipe-Sharing-Community-Server/src/app/modules/User/user.service.ts
@@ -0,0 +1,69 @@
+import { QueryBuilder } from '../../builder/QueryBuilder';
+import { USER_ROLE, UserSearchableFields } from './user.constant';
+import { TUser } from './user.interface';
+import { User } from './user.model';
+
+const createUser = async (payload: TUser) => {
+  const user = await User.create(payload);
+
+  return user;
+};
+
+const getAllUsersFromDB = async (query: Record<string, unknown>) => {
+  const users = new QueryBuilder(
+    User.find({ role: { $ne: USER_ROLE.ADMIN } }),
+    query
+  )
+    .fields()
+    .paginate()
+    .sort()
+    .filter()
+    .search(UserSearchableFields);
+
+  const result = await users.modelQuery;
+
+  return result;
+};
+
+const getAllAdminsFromDB = async (query: Record<string, unknown>) => {
+  const users = new QueryBuilder(
+    User.find({ role: { $eq: USER_ROLE.ADMIN } }),
+    query
+  )
+    .fields()
+    .paginate()
+    .sort()
+    .filter()
+    .search(UserSearchableFields);
+
+  const result = await users.modelQuery;
+
+  return result;
+};
+
+const getSingleUserFromDB = async (id: string) => {
+  const user = await User.findById(id);
+
+  return user;
+};
+
+const updateUserStatus = async (id: string, status: string) => {
+  // const user = await User.findById(id);
+  const query = { _id: id };
+  const option = { upsert: false };
+  const updatedDoc = {
+    $set: { status },
+  };
+
+  const result = await User.updateOne(query, updatedDoc, option);
+
+  return result;
+};
+
+export const UserServices = {
+  createUser,
+  getAllUsersFromDB,
+  getSingleUserFromDB,
+  getAllAdminsFromDB,
+  updateUserStatus,
+};
