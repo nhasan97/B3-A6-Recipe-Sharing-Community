e8bd1574d4b8912736dfbaa6b79e4575ce05bb0f
nhasan97 <nh6069325@gmail.com>
2024-10-10 08:20:55 +0600

    added scripts form build to test in package.json, added user in Express Namespace in index.d.ts in interfaces directory, created registerUser, loginUser, changePassword and refreshToken controller functions in auth.controller.ts, created TLoginUser and TRegisterUser types in auth.inteface.ts, created registerUser, loginUser, changePassword and refreshToken service functions in auth.service.ts, created registerValidationSchema, loginValidationSchema, changePasswordValidationSchema and refreshTokenValidationSchema in auth.validation.ts, created four post routes in auth.route.ts, created catchAsync, deleteImageFromCloudinary, sendResponse and verifyToken util functions, created ImageFileZodSchema and ImageFilesArrayZodSchema in image.validation.ts

---
+++ B3-A6-Recipe-Sharing-Community-Server/src/app/modules/Auth/auth.validation.ts
@@ -0,0 +1,50 @@
+import { z } from 'zod';
+import { USER_ROLE } from '../User/user.constant';
+
+const registerValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+    }),
+    email: z.string({
+      required_error: 'Email is required',
+    }),
+    password: z.string({ required_error: 'Password is required' }),
+    mobileNumber: z.string({ required_error: 'Mobile number is required' }),
+    role: z.string().optional().default(USER_ROLE.USER),
+    profilePhoto: z.string(),
+  }),
+});
+
+const loginValidationSchema = z.object({
+  body: z.object({
+    email: z.string({
+      required_error: 'Email is required',
+    }),
+    password: z.string({ required_error: 'Password is required' }),
+  }),
+});
+
+const changePasswordValidationSchema = z.object({
+  body: z.object({
+    oldPassword: z.string({
+      required_error: 'Old password is required',
+    }),
+    newPassword: z.string({ required_error: 'Password is required' }),
+  }),
+});
+
+const refreshTokenValidationSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh token is required!',
+    }),
+  }),
+});
+
+export const AuthValidation = {
+  registerValidationSchema,
+  loginValidationSchema,
+  changePasswordValidationSchema,
+  refreshTokenValidationSchema,
+};
