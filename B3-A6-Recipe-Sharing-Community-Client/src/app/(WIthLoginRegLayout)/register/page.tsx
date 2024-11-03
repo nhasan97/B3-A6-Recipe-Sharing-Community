b49c48ec99169ae86f4c13678a6319ffff7093a2
nhasan97 <nh6069325@gmail.com>
2024-11-03 09:21:53 +0600

    redesigned registration page, added reg.avif image resource and used it as backbground for registration page, made the registration page responsive and added button text changing feature based on condition

--- B3-A6-Recipe-Sharing-Community-Client/src/app/(WIthLoginRegLayout)/register/page.tsx
+++ B3-A6-Recipe-Sharing-Community-Client/src/app/(WIthLoginRegLayout)/register/page.tsx
@@ -4,7 +4,7 @@ import FXForm from "@/src/components/form/FXForm";
 import FXInput from "@/src/components/form/FXInput";
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import registerValidationSchema from "@/src/schemas/register.schema";
@@ -24,10 +24,15 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
-      <h3 className="my-2 text-2xl font-bold">Register with FoundX</h3>
-      <p className="mb-4">Help Lost Items Find Their Way Home</p>
-      <div className="w-[35%]">
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-[url('/assets/images/reg.avif')] bg-cover bg-center bg-no-repeat">
+      <div className="w-full sm:w-[70%] lg:w-[35%] bg-black/10 backdrop-blur-md p-10 rounded-lg">
+        <h3 className="my-2 text-2xl font-bold text-white">
+          Register to TasteTribe
+        </h3>
+        <p className="mb-4 text-white">
+          Where Every Dish Tells a Story – Share, Discover, and Savor!
+        </p>
+
         <FXForm
           //! Only for development
           defaultValues={{
@@ -53,11 +58,12 @@ const RegisterPage = () => {
           </div>
 
           <Button
-            className="my-3 w-full rounded-md bg-default-900 font-semibold text-default"
+            className="my-3 w-full bg-red-700 text-white"
             size="lg"
+            radius="md"
             type="submit"
           >
-            Register
+            {isPending ? "Registering..." : "Register"}
           </Button>
         </FXForm>
         <div className="text-center">
